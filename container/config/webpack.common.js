500ee15faf001e4011059d7077d652f301c4ca06
Sebastian <sebastian.soplan@davinci.edu.ar>
2021-03-07 02:43:13 -0300

    Adding features webpack config

--- container/config/webpack.common.js
+++ container/config/webpack.common.js
@@ -1,17 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
-    module: {
-        rules : [
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                         presets: ['@babel/preset-react','@babel/preset-env'],
-                         plugins: ['@babel/plugin-transform-runtime']
-                    }
-                }
-            }
-        ]
-    }
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
+  }
 }
